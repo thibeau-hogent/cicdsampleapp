32d64680c94bdc2ff668b64aa2fe5c678ad34ef3
thibeau-hogent <160587279+thibeau-hogent@users.noreply.github.com>
2025-10-23 11:48:32 +0200

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -10,7 +10,7 @@ cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
 cat > tempdir/Dockerfile << _EOF_
-FOM python
+FROM python
 RUN pip install flask
 COPY  ./static /home/myapp/static/
 COPY  ./templates /home/myapp/templates/
